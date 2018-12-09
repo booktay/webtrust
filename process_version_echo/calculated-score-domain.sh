e4cf87b0479dff72363dee4551b0703975d00662
Bell001 <bell849948@gmail.com>
2018-12-09 07:24:20 -0500

    [ADD] calculated file

---
+++ process_version_echo/calculated-score-domain.sh
@@ -0,0 +1,307 @@
+#!/bin/bash
+#------------------------/FORMAT/------------------------------------------#
+
+#url,http,code,hsts,https,scode,shsts,CertificateStatus,SignatureStatus,Expired(days),Fingerprint,SSLv2,SSLv3,TLS1,TLS1.1,TLS1.2,TLS1.3,NPN/SPDY,ALPN/HTTP2
+#www.nu.ac.th,yes,301,no,yes,200,max-age=31536000,good,no,565,SHA1,no,no,no,yes,yes,no,yes,yes
+
+#--------------------------------------------------------------------------#
+
+#clean 
+file_use=temp-test.txt
+DATE_WITH_TIME_s=`date "+%Y%m%d-%H%M%S"`
+rm output.txt output-1.txt temp output-3.txt  output-4.txt temp_2 temp-a 2>/dev/null
+#######################INPUT FILE#######################
+cp $file_use fix-use-gor-test-pro.txt
+active_https=""
+inactive_https=""
+active_http=""
+inactive_http=""
+#cat fix-use-gor-test.txt | cut -d ',' -f 3 | sort | uniq -c
+
+#Active-Inactive
+#---code---#
+awk 'BEGIN{OFS=FS=","} $3=="100" || $3=="101" || $3=="102" || $3=="103" || $3=="200" || $3=="201" || $3=="202" || $3=="203" || $3=="204" || $3=="205" || $3=="206" || $3=="207" || $3=="208" || $3=="218" || $3=="226" || $3=="301" || $3=="301" || $3=="302" || $3=="303" || $3=="304" || $3=="305" || $3=="306" || $3=="307" || $3=="308" {$3="active"}{print}' fix-use-gor-test-pro.txt >> output.txt
+awk 'BEGIN{OFS=FS=","} $3!="active" {$3="inactive"}{print}' output.txt >> output-1.txt
+cat output-1.txt | cut -d ',' -f 3 | sort | uniq -c >> temp
+#---scode---#
+awk 'BEGIN{OFS=FS=","} $6=="100" || $6=="101" || $6=="102" || $6=="103" || $6=="200" || $6=="201" || $6=="202" || $6=="203" || $6=="204" || $6=="205" || $6=="206" || $6=="207" || $6=="208" || $6=="218" || $6=="226" || $6=="301" || $6=="301" || $6=="302" || $6=="303" || $6=="304" || $6=="305" || $6=="306" || $6=="307" || $6=="308" {$6="active"}{print}' fix-use-gor-test-pro.txt >> output-3.txt
+awk 'BEGIN{OFS=FS=","} $6!="active" {$6="inactive"}{print}' output-3.txt >> output-4.txt
+cat output-4.txt | cut -d ',' -f 6 | sort | uniq -c >> temp_2
+
+#Score Basic statistic web (30%)
+#Active-Inactive (10)
+
+check_1n=$( cat temp | xargs | cut -d ' ' -f 1 )
+check_1=$( cat temp | xargs | cut -d ' ' -f 2 )
+check_2n=$( cat temp | xargs | cut -d ' ' -f 3 )
+check_2=$( cat temp | xargs | cut -d ' ' -f 4 )
+check_1n_2=$( cat temp_2 | xargs | cut -d ' ' -f 1 )
+check_1_2=$( cat temp_2 | xargs | cut -d ' ' -f 2 )
+check_2n_2=$( cat temp_2 | xargs | cut -d ' ' -f 3 )
+check_2_2=$( cat temp_2 | xargs | cut -d ' ' -f 4 )
+active_http=0
+inactive_http=0
+active_http=0
+inactive_http=0 
+
+if [ "${check_1}"  == "active" ];
+then
+     active_http=$(echo ${check_1n});
+     inactive_http=$(echo ${check_2n});
+else
+     active_http=$(echo ${check_2n});
+     inactive_http=$(echo ${check_1n});
+fi;
+
+if [ "${check_1_2}"  == "active" ];
+then
+     active_https=$(echo ${check_1n_2});
+     inactive_https=$(echo ${check_2n_2});
+else
+     active_https=$(echo ${check_2n_2});
+     inactive_https=$(echo ${check_1n_2});
+fi;
+
+if [ "${active_http}"  == "" ];
+then
+     active_http=0
+fi;
+if [ "${inactive_https}"  == "" ];
+then
+     inactive_https=0
+fi;
+if [ "${inactive_http}"  == "" ];
+then
+     inactive_http=0
+fi;
+if [ "${active_https}"  == "" ];
+then
+     active_https=0
+fi;
+all_num=$(($active_http+$inactive_http))
+score1_1=$((active_http*4/all_num))
+score1_2=$((active_https*6/all_num))
+score1=$((score1_1+score1_2))
+
+#echo "Part 1 Active-Inactive is" ${score1} "/10"
+
+#Base on type (20%)
+#HSTS+HTTPS (20)
+#HTTP+HTTPS (15)
+#Only HTTPS (10)
+#Only HTTP (0)
+#score =  (flag-type-per-web*score)/all_num
+count_hsts_https=0
+count_http_https=0
+count_only_https=0
+count_only_http=0
+count_certificate=0
+count_no_certificate=0
+#No_HSTS_HTTPS=$( cat fix-use-gor-test-pro.txt | cut -d ',' -f 7 | sort | uniq -c | grep no | xargs | cut -d ' ' -f 1 )
+#HSTS_HTTPS=$((all_num-No_HSTS_HTTPS))
+
+while read -r p; do
+     echo $p >> temp-a
+     v_http=$( cut -d ',' -f 2 temp-a)
+     v_hsts=$( cut -d ',' -f 4 temp-a)
+     v_https=$( cut -d ',' -f 5 temp-a) 
+     v_shsts=$( cut -d ',' -f 7 temp-a)
+     v_certificate=$( cut -d ',' -f 8 temp-a )
+     ####echo $v_http $v_hsts $v_https $v_shsts 
+     if [ "${v_https}" == "yes" ] && [ "${v_shsts}" != "no" ];
+     then
+          #count_hsts_https=$((count_hsts_https+1)) 
+          ((++count_hsts_https))
+     elif [ "${v_https}" == "yes" ] && [ "${v_http}" == "yes" ];
+     then
+          #count_http_https=$((count_http_https+1))
+          ((++count_http_https))
+     elif [ "${v_https}" == "yes" ] && [ "${v_http}" == "no" ];
+     then
+          #count_only_https=$((count_only_https+1))
+          ((++count_only_https))
+     elif [ "${v_https}" == "no" ] && [ "${v_http}" == "yes" ];
+     then
+          #count_only_http=$((count_only_http+1))
+          ((++count_only_http))
+     fi;            
+     if [ "${v_certificate}" != "-" ];
+     then
+          ((++count_certificate))
+     else
+          ((++count_no_certificate))
+     fi;
+     ####echo $count_hsts_https $count_http_https $count_only_https $count_only_http 
+     rm temp-a
+done < fix-use-gor-test-pro.txt
+
+####echo $count_hsts_https $count_http_https $count_only_https $count_only_http
+score2=$((count_hsts_https*20+count_http_https*15+count_only_https*10+count_only_http*0))
+score2=$((score2/all_num))
+
+#echo "Part 2 Basic status is" ${score2} "/20"
+
+#cat fix-use-gor-test.txt | cut -d ',' -f 1 | sort | uniq -c  | grep yes | cut -d ' ' -f 2
+
+#Score Domain (30%)
+#- Revolk 0 | not 10 
+#cat fix-use-gor-test.txt | cut -d ',' -f 8 | sort | uniq -c
+
+
+#- Expired 
+#<7 | 2.5
+#>=7, <30 | 5
+#>=30 | 10
+#cat fix-use-gor-test.txt | cut -d ',' -f 10 | sort | uniq -c
+
+
+#- valid 10 | invalid 0
+### ocsp test with PKI certificate
+### next change be from openssl (openssl s_client -connect example.com:443 -servername example.com) use [Verify return code: 0 (ok)]
+#cat fix-use-gor-test.txt | cut -d ',' -f 9 | sort | uniq -c
+
+count_valid=0
+count_u_revoke=0
+count_e_h=0
+count_e_m=0
+count_e_l=0
+count_ee=0
+re='^[0-9]+$'
+
+while read -r p; do
+     echo $p >> temp-a
+
+     v_valid=$( cut -d ',' -f 9 temp-a)
+     v_u_revoke=$( cut -d ',' -f 8 temp-a)
+     v_expired=$( cut -d ',' -f 10 temp-a)
+
+     ####echo $v_sslv2 $v_sslv3 $v_tls1 $v_tls1_1 $v_tls1_2 $v_tls1_3
+     if [ "${v_valid}" == "OK" ];
+     then
+          #count_hsts_https=$((count_hsts_https+1))
+          ((++count_valid))
+     fi;
+
+     if [ "${v_u_revoke}" == "good" ];
+     then
+          #count_hsts_https=$((count_hsts_https+1))
+          ((++count_u_revoke))
+     fi;
+
+     if ! [[ $v_expired =~ $re ]] ;
+     then 
+          ((++count_ee))
+     elif [ $v_expired -ge 30 ];
+     then
+          ((++count_e_h))
+     elif [ $v_expired -ge 7 ];
+     then
+          ((++count_e_m))
+     elif [ $v_expired -lt 7 ] && [ $v_expired -gt 0 ];
+     then
+          ((++count_e_l))
+     fi;
+
+     ####echo $count_tls1_2_tls1_3 $count_tls1_1 $count_sslv3_tls1 $count_sslv2
+     rm temp-a
+done < fix-use-gor-test-pro.txt
+
+score3=$((count_u_revoke*10))
+score3=$((score3/all_num))
+score4=$((count_e_h*10+count_e_m*5+count_e_l*(5/2)))
+score4=$((score4/all_num))
+score5=$((count_valid*10))
+score5=$((score5/all_num))
+
+#echo "Part 3 Revoke status is" ${score3} "/10"
+#echo "Part 4 Expired is" ${score4} "/10"
+#echo "Part 5 Valid-Invalid is" ${score5} "/10"
+
+#Score Protocol (40%)
+#40% tls1.2,1.3
+#30% tls 1.1
+#20% tls 0 ssl 3
+#10% ssl 2
+#0% else
+count_tls1_2_tls1_3=0
+count_tls1_1=0
+count_sslv3_tls1=0
+count_sslv2=0
+
+while read -r p; do
+     echo $p >> temp-a
+
+     v_sslv2=$( cut -d ',' -f 12 temp-a)
+     v_sslv3=$( cut -d ',' -f 13 temp-a)
+     v_tls1=$( cut -d ',' -f 14 temp-a)
+     v_tls1_1=$( cut -d ',' -f 15 temp-a)
+     v_tls1_2=$( cut -d ',' -f 16 temp-a)
+     v_tls1_3=$( cut -d ',' -f 17 temp-a)
+     
+     ####echo $v_sslv2 $v_sslv3 $v_tls1 $v_tls1_1 $v_tls1_2 $v_tls1_3
+     if [ "${v_tls1_2}" == "yes" ] || [ "${v_tls1_3}" == "yes" ];
+     then
+          #count_hsts_https=$((count_hsts_https+1))
+          ((++count_tls1_2_tls1_3))
+     elif [ "${v_tls1_1}" == "yes" ];
+     then
+          #count_http_https=$((count_http_https+1))
+          ((++count_tls1_1))
+     elif [ "${v_sslv3}" == "yes" ] || [ "${v_tls1}" == "yes" ];
+     then
+          #count_only_https=$((count_only_https+1))
+          ((++count_sslv3_tls1))
+     elif [ "${v_sslv2}" == "yes" ];
+     then
+          #count_only_http=$((count_only_http+1))
+          ((++count_sslv2))
+     fi;
+     ####echo $count_tls1_2_tls1_3 $count_tls1_1 $count_sslv3_tls1 $count_sslv2
+     rm temp-a
+done < fix-use-gor-test-pro.txt
+
+score6=$((count_tls1_2_tls1_3*40+count_tls1_1*30+count_sslv3_tls1*20+count_sslv2*10))
+score6=$((score6/all_num))
+
+#echo "Part 6 Protocol type is" ${score6} "/40"
+
+#Result Score (100%)
+
+total_score=$((score1+score2+score3+score4+score5+score6))
+#echo "Total score is" ${total_score} "/100"
+
+domain_grade=""
+if [ $total_score -ge 70 ] ;
+then
+          domain_grade="A"
+elif [ $total_score -ge 60 ];
+then
+          domain_grade="B+"
+elif [ $total_score -ge 50 ];
+then
+          domain_grade="B"
+elif [ $total_score -ge 40 ];
+then
+          domain_grade="C+"
+elif [ $total_score -ge 30 ];
+then
+          domain_grade="C"
+elif [ $total_score -ge 20 ];
+then
+          domain_grade="D"
+else
+          domain_grade="F"
+fi;
+
+#echo "url,http,code,hsts,https,scode,shsts,CertificateStatus,SignatureStatus,Expired(days),Fingerprint,SSLv2,SSLv3,TLS1,TLS1.1,TLS1.2,TLS1.3,NPN/SPDY,ALPN/HTTP2" >> v2_test.txt
+domain=$( cat fix-use-gor-test-pro.txt | cut -d ',' -f 1 | rev | cut -d'.' -f 1-2 | rev | sort | uniq -c | xargs | cut -d' ' -f 2 )
+#echo "Your domain is" ${domain} "Grade is" ${domain_grade}
+
+#Timestamp
+DATE_WITH_TIME_f=`date "+%Y%m%d-%H%M%S"`
+#echo "Start :" ${DATE_WITH_TIME_s} "Calculated :" ${DATE_WITH_TIME_f}
+temp=$( echo $active_http","$active_https","$inactive_http","$inactive_https","$domain_grade","${count_certificate}","${count_no_certificate}","${DATE_WITH_TIME_f} )
+echo $active_http","$active_https","$inactive_http","$inactive_https","$domain_grade","${count_certificate}","${count_no_certificate}","${DATE_WITH_TIME_f}","${score1}","${score2}","${score3}","${score4}","${score5}","${score6}","${domain_grade}","${total_score}
+mkdir -p ./result-score-domain
+echo $active_http","$active_https","$inactive_http","$inactive_https","$domain_grade","${count_certificate}","${count_no_certificate}","${DATE_WITH_TIME_f}","${score1}","${score2}","${score3}","${score4}","${score5}","${score6}","${domain_grade}","${total_score} >> ${DATE_WITH_TIME_f}"-"$domain".txt"
+mv ${DATE_WITH_TIME_f}"-"$domain".txt" ./result-score-domain
---
+++ process_version_echo/readme.md
@@ -0,0 +1,11 @@
+sh calculated-all.sh [[domain file]] 
+
+------------------------------------
+Domain file format
+------------------------------------
+www.cu.ac.th
+www.ku.ac.th
+...
+
+example in ./domain-file 
+
